a21bf0156c76693647e012ee23f61307f1246317
fermayo <fermayo@gmail.com>
2016-06-30 16:06:42 +0200

    Escape contents

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine
 MAINTAINER fermayo@gmail.com
 
-RUN apk --update add curl jq
+RUN apk --update add curl jq python
 ADD run.sh /run.sh
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-CONTENT=$(cat -)
+CONTENT=$(cat - | python -c 'import json,sys; print json.dumps(sys.stdin.read())')
 BODY=$(cat <<EOF
 {
   "public": false,
   "files": {
     "out.txt": {
-      "content": "$CONTENT"
+      "content": $CONTENT
     }
   }
 }
 EOF
 )
+
 curl -sS -X POST https://api.github.com/gists -d "$BODY" \
 	-H 'Accept: application/vnd.github.v3+json' \
 	-H 'Content-Type: application/json; charset=utf-8' \
