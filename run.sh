7aaf78455eac93c2687cbac7ae913ec3952b65ef
fermayo <fermayo@gmail.com>
2016-06-30 16:14:58 +0200

    Use a buffer file

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONTENT=$(cat - | python -c 'import json,sys; print json.dumps(sys.stdin.read())')
-BODY=$(cat <<EOF
+cat <<EOF > data.txt
 {
   "public": false,
   "files": {
@@ -10,9 +10,8 @@ BODY=$(cat <<EOF
   }
 }
 EOF
-)
 
-curl -sS -X POST https://api.github.com/gists -d "$BODY" \
+curl -sS -X POST https://api.github.com/gists -d @data.txt \
 	-H 'Accept: application/vnd.github.v3+json' \
 	-H 'Content-Type: application/json; charset=utf-8' \
 	| jq -r '.html_url'
